a23cbdc3d99784e32b7ed1c8c62a81308e7c86f6
Josh Callender <jcallender@change.org>
2015-02-26 17:15:31 -0800

    update version 02 with rendr 1.0.1 and rendr-handlebars 1.0.0

--- 02_middleware/index.js
+++ 02_middleware/index.js
@@ -1,16 +1,11 @@
 var express = require('express')
   , rendr = require('rendr')
   , config = require('config')
+  , compress = require('compression')
+  , bodyParser = require('body-parser')
+  , serveStatic = require('serve-static')
   , mw = require('./server/middleware')
   , app = express();
-
-/**
- * Initialize Express middleware stack.
- */
-app.use(express.compress());
-app.use(express.static(__dirname + '/public'));
-app.use(express.logger());
-app.use(express.bodyParser());
 
 /**
  * Initialize our Rendr server.
@@ -21,45 +16,16 @@ var server = rendr.createServer({
 });
 
 /**
- * The `addLocaleToRequest` middleware can go outside of a `server.configure()` block
- * because it doesn't access `req.rendrApp`. This example middleware just adds a
- * `req.locale` property based on the query string. Because the same exact `req`
- * object is passed to each middleware, any middleware that follow
- * `addLocaleToRequest` can access `req.locale`, including the middleware added to
- * the Rendr server's internal Express app in a `server.configure()` block.
+ * Initialize Express middleware stack.
  */
-app.use(mw.addLocaleToRequest());
-
-/**
- * Demonstrate how to use Express' `res.locals` to pass additional data to the
- * layout template. You can see how this value is used in the layout file,
- * `app/templates/__layout.hbs`. You can also use Express' `app.locals` for
- * values that are not request-specific.
- */
-app.use(function(req, res, next) {
-  res.locals.repoUrl = 'https://github.com/rendrjs/rendr';
-  next();
+server.configure(function (expressApp) {
+  expressApp.use(compress());
+  expressApp.use(serveStatic(__dirname + '/public'));
+  expressApp.use(bodyParser.json());
+  expressApp.use(mw.fetchDataForApp({apiKeyForFakeService: 'sup3rs3cr3t'}));
 });
 
-/**
-  * To mount Rendr, which owns its own Express instance for better encapsulation,
-  * simply add `server` as a middleware onto your Express app.
-  * This will add all of the routes defined in your `app/routes.js`.
-  * If you want to mount your Rendr app onto a path, you can do something like:
-  *
-  *     app.use('/my_cool_app', server);
-  */
-app.use(server);
-
-server.configure(function(rendrExpressApp) {
-  /**
-   * The `fetchDataForApp` middleware has to go in a `server.configure()` block
-   * because it access `req.rendrApp`, which is equivalent to `this.app` in your
-   * models, views, and controllers.
-   */
-  rendrExpressApp.use(mw.fetchDataForApp({apiKeyForFakeService: 'sup3rs3cr3t'}));
-});
-
+app.use('/', server.expressApp);
 
 /**
  * Start the Express server.
